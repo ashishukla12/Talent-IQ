c32af1660746b7378cc890a325385dcc056865eb
Ashish shukla <ashishukla31405@gmail.com>
2026-06-20 16:08:27 +0530

    Fix render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,29 @@
-//const express = require('express');
+import express from "express";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
-import express from 'express';
-import path from 'path';
-import { ENV } from './lib/env.js';
-import { connectDB } from './lib/db.js';
 const app = express();
 
-const __dirname = path.resolve()
+// Middleware
+app.use(express.json());
 
-
-// make our app ready for deployment
-if(ENV.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-  app.get('/{*any}', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-  })
-}
-
+// Health Check Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Backend is running 🚀",
+  });
+});
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => 
-      console.log(`Server is running on port ${ENV.PORT}`)
-    )
-  }catch (error) {
-    console.error('Error starting the server:', error);
+
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
     process.exit(1);
   }
 };
