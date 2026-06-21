bcc8bd29809c26d98d301aef1e7f42081069f4f9
Ashish shukla <ashishukla31405@gmail.com>
2026-06-21 16:22:23 +0530

    Add health route

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -5,7 +5,7 @@ import User from "../models/User.js";
 export const inngest = new Inngest({ id: "talent-iq" });
 
 const syncUser = inngest.createFunction(
-  { id: "sync-user", event: "clerk/user.created" },
+  { id: "sync-user" }, { event: "clerk/user.created" },
   async ({ event }) => {
       await connectDB();
       const {id, email_addresses, first_name, last_name, image_url} = event.data;
@@ -22,7 +22,7 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user-from-db", event: "clerk/user.deleted" },
+  { id: "delete-user-from-db" }, { event: "clerk/user.deleted" },
   async ({ event }) => {
       await connectDB();
 
