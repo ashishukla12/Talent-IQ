ffcef0b5b8d5298472b6e0b81ab12caf0c416c85
Ashish shukla <ashishukla31405@gmail.com>
2026-06-23 14:44:06 +0530

    refractoring sessionController.js

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -106,7 +106,7 @@ export async function joinSession(req, res) {
     if (!session) return res.status(404).json({ message: "Session not found" });
 
     // check if session is already full - has a participant
-    if (session.participant) return res.status(404).json({ message: "Session is full" });
+    if (session.participant) return res.status(409).json({ message: "Session is full" });
 
     session.participant = userId;
     await session.save();
@@ -140,8 +140,6 @@ export async function endSession(req, res) {
       return res.status(400).json({ message: "Session is already completed" });
     }
 
-    session.status = "completed";
-    await session.save();
 
     // delete stream video call
     const call = streamClient.video.call("default", session.callId);
@@ -150,6 +148,9 @@ export async function endSession(req, res) {
     // delete stream chat channel
     const channel = chatClient.channel("messaging", session.callId);
     await channel.delete();
+
+    session.status = "completed";
+    await session.save();    
 
     res.status(200).json({
       session,
