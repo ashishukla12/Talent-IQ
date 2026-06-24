4f6145cb6483f41d65030a0cd8658e339c03d213
Ashish shukla <ashishukla31405@gmail.com>
2026-06-24 12:54:27 +0530

    Remove prisma import from chat controller

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,20 +1,19 @@
-import { UNABLE_TO_FIND_POSTINSTALL_TRIGGER_JSON_SCHEMA_ERROR } from "@prisma/client/scripts/postinstall.js";
+import { chatClient } from "../lib/stream.js";
 
-export async function getStreamToken(req, res){
+export async function getStreamToken(req, res) {
   try {
-    // Assuming you have a function to generate a Stream token
+    const token = chatClient.createToken(req.user.clerkId);
 
-    // use clerkId for stream (not mongoDB _id) => it should match the id we have in the stream dashboard for the user
-    const token = chatClient.createToken(req.user.clerkId)
-
-    res.status(200).json({    
+    res.status(200).json({
       token,
       userId: req.user.clerkId,
       userName: req.user.name,
-      userImage: req.user.image
-     })
-    } catch (error) {
-      console.error("Error generating Stream token:", error);
-      res.status(500).json({ error: "Failed to generate Stream token" });
-    }
+      userImage: req.user.profileImage,
+    });
+  } catch (error) {
+    console.error("Error generating Stream token:", error);
+    res.status(500).json({
+      error: "Failed to generate Stream token",
+    });
   }
+}
