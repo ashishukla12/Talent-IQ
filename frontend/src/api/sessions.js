2af7e41792d199902edf4eb85d3fd1b8ca7806cc
Ashish shukla <ashishukla31405@gmail.com>
2026-06-26 18:57:20 +0530

    Fix session problem

--- frontend/src/api/sessions.js
+++ frontend/src/api/sessions.js
@@ -2,34 +2,30 @@ import axiosInstance from "../lib/axios";
 
 export const sessionApi = {
   createSession: async (data) => {
-    const response = await axiosInstance.post("/sessions", data);
-    return response.data;
+    return (await axiosInstance.post("/api/session", data)).data;
   },
 
   getActiveSessions: async () => {
-    const response = await axiosInstance.get("/sessions/active");
-    return response.data;
+    return (await axiosInstance.get("/api/session/active")).data;
   },
+
   getMyRecentSessions: async () => {
-    const response = await axiosInstance.get("/sessions/my-recent");
-    return response.data;
+    return (await axiosInstance.get("/api/session/my-recent")).data;
   },
 
   getSessionById: async (id) => {
-    const response = await axiosInstance.get(`/sessions/${id}`);
-    return response.data;
+    return (await axiosInstance.get(`/api/session/${id}`)).data;
   },
 
   joinSession: async (id) => {
-    const response = await axiosInstance.post(`/sessions/${id}/join`);
-    return response.data;
+    return (await axiosInstance.post(`/api/session/${id}/join`)).data;
   },
+
   endSession: async (id) => {
-    const response = await axiosInstance.post(`/sessions/${id}/end`);
-    return response.data;
+    return (await axiosInstance.post(`/api/session/${id}/end`)).data;
   },
+
   getStreamToken: async () => {
-    const response = await axiosInstance.get(`/chat/token`);
-    return response.data;
+    return (await axiosInstance.get("/api/chat/token")).data;
   },
 };
